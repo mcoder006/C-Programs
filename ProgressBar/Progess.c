d73a7e4032ebb73a38c8c00beb1440e273c400da
Sushank Ghimire <mcoder006@gmail.com>
2024-05-06 13:30:43 +0545

    ProgressBar Completed

--- ProgressBar/Progess.c
+++ ProgressBar/Progess.c
@@ -1,9 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main() {
+void progressBar(int totalTime)
+{
+    const int barWidth = 50;
+    int i;
+    for (i = 0; i <= totalTime; i++)
+    {
+        float progress = (float)i / totalTime;
+        int pos = (int)(barWidth * progress);
 
-  printf("Hello, Progress Bar");
-
-  return 0;
+        printf("[");
+        for (int j = 0; j < barWidth; j++)
+        {
+            if (j < pos)
+                printf("=");
+            else if (j == pos)
+                printf(">");
+            else
+                printf(" ");
+        }
+        printf("] %d%%\r", (int)(progress * 100));
+        fflush(stdout);
+        sleep(1);
+    }
+    printf("\n");
 }
 
+int main()
+{
+    int times[] = {3, 5, 7}; 
+    int numTimes = sizeof(times) / sizeof(times[0]);
+
+    for (int i = 0; i < numTimes; i++)
+    {
+        printf("Progress bar for %d seconds:\n", times[i]);
+        progressBar(times[i]);
+        printf("\n");
+    }
+
+    return 0;
+}
